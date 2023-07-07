ef35d1cd49cdc6d97014097f48153e92605e8d91
pung8146 <pung8146@naver.com>
2023-07-07 14:04:35 +0900

    styled-components 적용

--- src/components/InputMD.tsx
+++ src/components/InputMD.tsx
@@ -1,5 +1,60 @@
 import React, { useState, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
+import styled from "styled-components";
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 2em;
+`;
+
+const TextArea = styled.textarea`
+  padding: 10px;
+  margin-bottom: 1em;
+  font-size: 16px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  min-width: 500px;
+`;
+
+const Button = styled.button`
+  cursor: pointer;
+  padding: 10px;
+  font-size: 16px;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  transition: all 0.3s;
+`;
+
+const SubmitButton = styled(Button)`
+  background-color: #007bff;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
+
+const ResetButton = styled(Button)`
+  margin-top: 0.5em;
+  background-color: #dc3545;
+
+  &:hover {
+    background-color: #c82333;
+  }
+`;
+
+const MarkdownWrapper = styled.div`
+  max-width: 800px;
+  padding: 1em;
+  background-color: white;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+  img {
+    width: 100%;
+  }
+`;
 
 export default function InputMD() {
   const [input, setInput] = useState("");
@@ -25,22 +80,20 @@ export default function InputMD() {
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <textarea
+      <Form onSubmit={handleSubmit}>
+        <TextArea
           value={input}
           onChange={handleChange}
           placeholder="MD형식의 문서를 붙여 넣어주세요"
           rows={10}
           cols={50}
         />
-        <button type="submit">변환하기</button>
-        <button type="reset">취소하기</button>
-      </form>
-      <div style={{ textAlign: "left" }}>
-        {" "}
-        {/* 추가된 스타일 */}
+        <SubmitButton type="submit">변환하기</SubmitButton>
+        <ResetButton type="reset">취소하기</ResetButton>
+      </Form>
+      <MarkdownWrapper style={{ textAlign: "left" }}>
         <ReactMarkdown>{markdown}</ReactMarkdown>
-      </div>
+      </MarkdownWrapper>
     </div>
   );
 }
